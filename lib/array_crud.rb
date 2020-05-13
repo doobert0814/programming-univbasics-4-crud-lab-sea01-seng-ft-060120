f0ed20cfe619321c2a2dfec36518651d70fd7133
Scot Sexton <rss3509@gmail.com>
2020-05-13 00:39:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,7 +16,8 @@ def add_element_to_end_of_array(array, element)
   
 end
 
-def add_element_to_start_of_array.unshift("Trumpet")
+def add_element_to_start_of_array 
+  add_element_to_start_of_array.unshift("Trumpet")
   
 end
 
